b863be49a640e2d5dfeb48466789d412054a554b
jadwro <jadwro@gmail.com>
2022-01-31 10:02:47 +0100

    30

--- cypress/integration/automation-test-store/vars.js
+++ cypress/integration/automation-test-store/vars.js
@@ -7,7 +7,7 @@ describe('Sprawdzanie zmiennych i jQuery', () => {
         cy.get('a[href*="product/category&path="]').contains('Skincare').click();
     });
 
-    it.only('Nawigacja do strony produktu z głównego menu', () => {
+    it('Nawigacja do strony produktu z głównego menu', () => {
         cy.visit('https://automationteststore.com/');        
         cy.get('a[href*="product/category&path="]').contains('Makeup').click();
 
@@ -16,6 +16,24 @@ describe('Sprawdzanie zmiennych i jQuery', () => {
             cy.log(headerText);
             console.log($headerText);
             console.log($headerText[0].textContent);
+            expect(headerText).is.eq('Makeup');
+        });
+    });
+
+    it.only('Walidacja Contact us', () => {
+        cy.visit('https://automationteststore.com/index.php?rt=content/contact');
+        
+        // Chaining
+        cy.contains('#ContactUsFrm', 'Contact Us Form').find('#field_11').should('contain', 'First name')
+
+        //jQuery
+        cy.contains('#ContactUsFrm', 'Contact Us Form').then(text => {
+            const firstNameText = text.find('#field_11').text();
+            expect(firstNameText).to.contain('First name');
+
+            cy.get('#field_11').then(scText => {
+                cy.log(scText.text());
+            });
         });
     });
 });
