b5325b43363213417196096a08c98517b8a9e1b0
Adam Jankowski <adam.jankowski@itmagination.com>
2022-02-03 15:57:55 +0100

    03.02

--- cypress/integration/webdriver-uni/checkboxes.js
+++ cypress/integration/webdriver-uni/checkboxes.js
@@ -1,10 +1,12 @@
 /// <reference types="Cypress" />
 
 describe('Weryfikacja checkboxów', () => {
-    it('Zaznacz i sprawdź checkbox', () => {
+    beforeEach(() => {
         cy.visit('https://webdriveruniversity.com');
         cy.get('#dropdown-checkboxes-radiobuttons').invoke('removeAttr', 'target').click({ force:true });
-        
+    });
+
+    it('Zaznacz i sprawdź checkbox', () => {
         cy.get('#checkboxes > :nth-child(1) > input').as('opt1');
         cy.get('#checkboxes > :nth-child(5) > input').as('opt3');
         cy.get('@opt1').check().should('be.checked');;
@@ -13,10 +15,7 @@ describe('Weryfikacja checkboxów', () => {
         cy.get('@opt3').uncheck().should('not.be.checked');
     });
 
-    it.only('Multiple checkbox', () => {
-        cy.visit('https://webdriveruniversity.com');
-        cy.get('#dropdown-checkboxes-radiobuttons').invoke('removeAttr', 'target').click({ force:true });
-        
+    it('Multiple checkbox', () => {
         cy.get('input[type="checkbox"]').check(["option-1", "option-2", "option-3", "option-4"]).should('be.checked');
     });
 });
--- cypress/integration/webdriver-uni/radios.js
+++ cypress/integration/webdriver-uni/radios.js
@@ -1,18 +1,17 @@
 /// <reference types="Cypress" />
 
 describe('Weryfikacja radio', () => {
-    it('Zaznacz i sprawdź radio', () => {
+    before(() => {
         cy.visit('https://webdriveruniversity.com');
         cy.get('#dropdown-checkboxes-radiobuttons').invoke('removeAttr', 'target').click({ force:true });
-        
+    });
+    
+    it('Zaznacz i sprawdź radio', () => {
         cy.get('#radio-buttons').find('input[type="radio"]').first().check();
         cy.get('#radio-buttons').find('input[type="radio"]').eq(1).check();
     });
 
-    it.only('Zaznacz i sprawdź radio z disabled', () => {
-        cy.visit('https://webdriveruniversity.com');
-        cy.get('#dropdown-checkboxes-radiobuttons').invoke('removeAttr', 'target').click({ force:true });
-        
+    it('Zaznacz i sprawdź radio z disabled', () => {        
         cy.get('#radio-buttons-selected-disabled').find('input[type="radio"]').first().check();
         cy.get('#radio-buttons-selected-disabled').find('input[type="radio"][value="cabbage"]').should('be.disabled');
         cy.get('#radio-buttons-selected-disabled').find('input[type="radio"]').eq(1).check({force:true});
