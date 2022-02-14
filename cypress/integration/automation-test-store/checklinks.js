86c35ed57d5aabdbc13b79b93be925646f1ce947
jadwro <jadwro@gmail.com>
2022-02-14 13:38:01 +0100

    links

---
+++ cypress/integration/automation-test-store/checklinks.js
@@ -0,0 +1,20 @@
+import HomePage from '../../support/pageObject/automationstore/Homepage';
+
+/// <reference types="Cypress" />
+
+describe('Dodawanie kilku elementów do koszyka', () => {
+    const homepage = new HomePage();
+
+    // beforeEach(() => {
+    //     // cy.visit('https://automationteststore.com/');        
+    //     // cy.get('a[href*="product/category&path="]').contains('Hair Care').click();
+    //     homepage.visitHomepage();
+    // });
+
+    it('klik link', () => {
+        cy.visit('https://www.jadwro.xce.pl')
+        cy.get('a:visible').filter('[href]').each((link) => {
+            cy.request(link.prop('href')).its('status').should('eq', 200)
+        });
+    });
+});
